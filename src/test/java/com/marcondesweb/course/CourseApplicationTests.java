586ae8b765d3da9a2e4ca47fa3aa9f74eb6d3edd
Marcondes-Goncalves <marcondesgoncalves159@gmail.com>
2023-08-07 15:13:56 -0300

    Project Created

---
+++ src/test/java/com/marcondesweb/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcondesweb.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
