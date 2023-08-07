3b79574e5e87d79f3f280e7155c48726479f1d21
Marcondes-Goncalves <marcondesgoncalves159@gmail.com>
2023-08-07 15:41:05 -0300

    User entity and resource

---
+++ src/main/java/com/marcondesweb/course/resources/UserResources.java
@@ -0,0 +1,29 @@
+package com.marcondesweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marcondesweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+    
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999999", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+
+
+
+
+
+
+
+
+
+
+}
