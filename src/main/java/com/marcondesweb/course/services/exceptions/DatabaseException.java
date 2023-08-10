256cabdc344b10f5f0c39e155ebaaddc6a74df78
Marcondes-Goncalves <marcondesgoncalves159@gmail.com>
2023-08-10 20:44:03 -0300

    Exception handling - delete

---
+++ src/main/java/com/marcondesweb/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,12 @@
+package com.marcondesweb.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+
+
+
+}
